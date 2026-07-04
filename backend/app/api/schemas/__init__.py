b1930d0f20ff251aabcf0800ea1b385ceaa7b354
ha08625-cloud <ha08625@googlemail.com>
2026-07-04 20:58:08 +0100

    Create schemas/__init__.py

---
+++ backend/app/api/schemas/__init__.py
@@ -0,0 +1,44 @@
+"""Pydantic schemas for the M3 API."""
+from .common import ValidationIssueOut
+from .rota import (
+    GenerateRotaIn,
+    GenerateRotaOut,
+    RotaOut,
+    RotaSessionOut,
+    SwapIn,
+    SwapOut,
+)
+from .clinic_type import (
+    ClinicTypeIn,
+    ClinicTypeOut,
+    DoctorEligIn,
+    DoctorEligOut,
+    RoomEligIn,
+    RoomEligOut,
+    ScheduleIn,
+    ScheduleOut,
+)
+from .doctor import (
+    DoctorDetailOut,
+    DoctorIn,
+    DoctorOut,
+    DoctorPatch,
+    PreferredRoomIn,
+    PreferredRoomOut,
+)
+from .leave import LeaveIn, LeaveOut
+from .duty import DutyIn, DutyOut
+from .room import RoomOut
+from .counter import ClinicCounterOut, SystemCounterOut
+
+__all__ = [
+    "ValidationIssueOut",
+    "GenerateRotaIn", "GenerateRotaOut", "RotaOut", "RotaSessionOut",
+    "SwapIn", "SwapOut",
+    "ClinicTypeIn", "ClinicTypeOut", "DoctorEligIn", "DoctorEligOut",
+    "RoomEligIn", "RoomEligOut", "ScheduleIn", "ScheduleOut",
+    "DoctorDetailOut", "DoctorIn", "DoctorOut", "DoctorPatch",
+    "PreferredRoomIn", "PreferredRoomOut",
+    "LeaveIn", "LeaveOut", "DutyIn", "DutyOut",
+    "RoomOut", "ClinicCounterOut", "SystemCounterOut",
+]
