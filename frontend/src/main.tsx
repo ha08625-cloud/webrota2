c979fb25b2efcec381f991933913068ce734bcc8
ha08625-cloud <ha08625@googlemail.com>
2026-07-09 09:38:00 +0100

    removed unused import from main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
  
 import { App } from "./App";
-import type { ApiError } from "./api/types";
 import { TokenGate } from "./auth/TokenGate";
 import "./index.css";
  
