6b03bd53a459e3f067fa3608e4b1b491d433926c
ha08625-cloud <ha08625@googlemail.com>
2026-07-18 13:58:05 +0100

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -31,6 +31,7 @@ from .master_rota import MasterRotaTemplate, MasterRotaSession
 from .rota import RotaConfig, GeneratedRota, RotaSession
 from .closure import PracticeClosure, RotaClosure
 from .generation_log import RotaGenerationLogEntry
+from .signature import DoctorSignature
 
 __all__ = [
     # enums
@@ -44,4 +45,5 @@ __all__ = [
     "MasterRotaSession", "RotaConfig", "GeneratedRota", "RotaSession",
     "RotaClinicCounterSnapshot", "RotaSystemCounterSnapshot",
     "PracticeClosure", "RotaClosure", "RotaGenerationLogEntry",
+    "DoctorSignature",
 ]
